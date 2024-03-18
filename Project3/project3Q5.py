9436508b4aa3bc7e8aee26f5f54e8bf57b5e9783
ashollerbach <ashollerbach@gmail.com>
2024-03-18 15:57:48 -0500

    questions 1, 2, and 4 work. questions 3 and 5 are in question

--- Project3/project3Q5.py
+++ Project3/project3Q5.py
@@ -12,7 +12,6 @@ hist_x, bin_xedges = np.histogram(x, bins=50)
 hist_y, bin_yedges = np.histogram(y, bins=50)
 
 X, Y = np.meshgrid(bin_xedges[:-1], bin_yedges[:-1])
-
 joint = hist_x[:, np.newaxis] * hist_y[np.newaxis, :]
 
 fig = plt.figure()
